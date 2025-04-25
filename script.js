841df78ef9c91a98d9230123a0a24d718c4d4a05
christian calderon <print.christiancalderon@gmail.com>
2025-04-25 06:37:37 -0700

    updated HTML

---
+++ script.js
@@ -0,0 +1,24 @@
+function countFlavors() {
+  const name = prompt("Enter your froyo flavors (separated by commas):");
+
+  const flavors = name.toLowerCase().split(",");
+
+  const flavorCounts = {};
+
+  for (let i = 0; i < flavors.length; i++) {
+    let flavor = flavors[i].trim();
+
+    if (flavorCounts[flavor]) {
+      flavorCounts[flavor] = flavorCounts[flavor] + 1;
+    } else {
+      flavorCounts[flavor] = 1;
+    }
+  }
+
+  console.log("Your Froyo Order:");
+  for (let flavor in flavorCounts) {
+    console.log(flavor + ": " + flavorCounts[flavor]);
+  }
+}
+
+countFlavors();
